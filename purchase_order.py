43f5f3e12cd8034f0f12d4f8d585a39d742ed578
Timo Talvitie <timo.talvitie@vizucom.com>
2015-03-16 13:54:48 +0200

    Initial commit - new freetext field for purchase orders

---
+++ purchase_order.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from openerp.osv import osv, fields
+from openerp.tools.translate import _
+import datetime
+
+class purchase_order(osv.Model):
+    
+    _inherit = 'purchase.order'
+
+    _columns = {
+        'internal_notes': fields.text('Internal Notes'),
+    }
